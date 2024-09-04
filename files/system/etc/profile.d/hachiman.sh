71dee2a8d205f358f7ac8f68559b9a332bbc7615
Ray Gomez <ray.rgomez@gmail.com>
2024-09-03 19:50:07 -0700

    Rename some global variables, add just command for upgrading.

--- files/system/etc/profile.d/hachiman.sh
+++ files/system/etc/profile.d/hachiman.sh
@@ -1,4 +1,7 @@
 #! /usr/bin/env sh
 
 # This sets the default location of the shared distrobox.ini file.
-export GLOBAL_DISTROBOX_INI="/usr/share/hachiman/distrobox/distrobox.ini"
+export HACHIMAN_DISTROBOX_INI="/usr/share/hachiman/distrobox/distrobox.ini"
+
+# This sets the default location of the distrobox home folders
+export HACHIMAN_DISTROBOX_HOME="${HOME}/.local/share/distrobox"
