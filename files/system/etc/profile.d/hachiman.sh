ab32ec68f6833ff281081300b639ce97af41a876
Ray Gomez <ray.rgomez@gmail.com>
2024-09-03 21:43:32 -0700

    Possible bug in distrobox.ini worked around, disabled ryzen-smu as it doesn't support 7840U codename phoenix, added todo comment.

--- files/system/etc/profile.d/hachiman.sh
+++ files/system/etc/profile.d/hachiman.sh
@@ -5,3 +5,6 @@ export HACHIMAN_DISTROBOX_INI="/usr/share/hachiman/distrobox/distrobox.ini"
 
 # This sets the default location of the distrobox home folders
 export HACHIMAN_DISTROBOX_HOME="${HOME}/.local/share/distrobox"
+
+# Hachiman uses podman so make sure DOCKER_HOST is set the the users .sock file
+export DOCKER_HOST=$(podman info --format '{{.Host.RemoteSocket.Path}}')
