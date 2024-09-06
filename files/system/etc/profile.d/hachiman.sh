33cd7efaaf5e3366854904568a71f5f7efd3df31
Ray Gomez <ray.rgomez@gmail.com>
2024-09-06 13:37:05 -0700

    Fix DOCKER_HOST issue, update default wallpaper.

--- files/system/etc/profile.d/hachiman.sh
+++ files/system/etc/profile.d/hachiman.sh
@@ -7,4 +7,4 @@ export HACHIMAN_DISTROBOX_INI="/usr/share/hachiman/distrobox/distrobox.ini"
 export HACHIMAN_DISTROBOX_HOME="${HOME}/.local/share/distrobox"
 
 # Hachiman uses podman so make sure DOCKER_HOST is set the the users .sock file
-export DOCKER_HOST=$(podman info --format '{{.Host.RemoteSocket.Path}}')
+export DOCKER_HOST=unix://$(podman info --format '{{.Host.RemoteSocket.Path}}')
