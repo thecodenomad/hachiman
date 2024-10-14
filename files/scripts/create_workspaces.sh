6f5b55a1646682c7da7136c8556735b72dfa799b
Ray Gomez <ray.rgomez@gmail.com>
2024-10-13 18:16:04 -0700

    Fix issues with create workspace script switching to json.

--- files/scripts/create_workspaces.sh
+++ files/scripts/create_workspaces.sh
@@ -1,36 +1,84 @@
 #!/usr/bin/env bash
 
-# Tell this script to exit if there are any errors.
-# You should have this in every custom script, to ensure that your completed
-# builds actually ran successfully without any errors!
+##################################################################
+#                                                                #
+# Simple script to parse json file of workspaces. A workspace is #
+# defined as a set of repos that should be cloned to a specific  #
+# folder location.                                               #
+#                                                                #
+##################################################################
+
 set -oue pipefail
 
-# This shell script is used to create dev workspaces.
-# A workspace is simply a folder structure where git repos are stored.
+# TODO: Support yaml
+DEFAULT_CONFIG_PATH="${HOME}/.local/hachiman/workspaces.json"
 
-# DEFAULT_WORKSPACE_CONFIG is a yaml file that should be captured by
-# dotfile configuration management (chezmoi, stow, etc)
-DEFAULT_WORKSPACE_CONFIG=$HOME/.local/hachiman/workspaces.yml
+# Allow override if the user chooses
+eval WORKSPACE_CONFIG="${WORKSPACE_CONFIG:-"${DEFAULT_CONFIG_PATH}"}"
 
-WORKSPACE=$0
+# User didn't provide a workspace
+if [[ $# -eq 0 ]]; then
+    echo "Please provide a workspace!"
+    exit 1
+fi
 
-# Does the workspace exist
-if ! yq '.workspace | strenv(WORKSPACE)' $HOME/.local/hachiman/workspaces.yml; then
-  echo "Workspace ${WORKSPACE} doesn't exist in ${DEFAULT_WORKSPACE_CONFIG}"
+WORKSPACE="${1}"
+
+check_and_clone() {
+    repo=${1}
+    base_folder=${2}
+    repo_name=$(basename ${repo} | sed s/.git//g )
+    repo_path="${base_folder}/${repo_name}"
+
+    echo "Checking state of repo ${repo}..."
+
+    # Check base folder path
+    if [[ ! -e "${base_folder}" ]]; then
+        echo "  Base Folder path '${base_folder}' doesn't exist"
+        read -p "  Create path? (Y/n) : " response
+
+        response=${response,,} # tolower
+        if [[ $response =~ ^(yes|y| ) ]] || [[ -z $response ]]; then
+            mkdir -p ${base_folder}
+        else
+            echo "  Base folder doesn't exist, skipping cloning repo ${repo}"
+            return
+        fi
+    fi
+
+    # Check full repo path
+    if [[ -e "${repo_path}" ]]; then
+      echo "  Path ${repo_path} already exists, skipping..."
+      return
+    fi
+
+    pushd "${base_folder}" > /dev/null
+        echo "  Entering ${base_folder}"
+        git clone "git@${repo}"
+    popd > /dev/null
+}
+
+# Config check
+if [[ ! -e "${WORKSPACE_CONFIG}" ]]; then
+    echo "Workspace config not found at ${WORKSPACE_CONFIG}"
+    exit 1
+fi
+
+# Workspace check
+if ! jq -e ".workspaces[env.WORKSPACE]" ${WORKSPACE_CONFIG} > /dev/null; then
+  echo "Workspace ${WORKSPACE} doesn't exist in ${WORKSPACE_CONFIG}"
   exit 1
 fi
 
-REPO=$(yq '.workspaces[] | select(strenv(WORKSPACE)) | .repo' $DEFAULT_WORKSPACE_CONFIG)
-eval BASE_FOLDER=$(yq '.workspaces[] | select(strenv(WORKSPACE)) | .base_folder' $DEFAULT_WORKSPACE_CONFIG)
+# Collect the repos to create
+REPOS=($(jq -r ".workspaces[env.WORKSPACE].[] | @base64" ${WORKSPACE_CONFIG}))
 
-# Does the repo already exist?
-REPO_NAME=$(basename ${REPO} | sed s/.git//)
-if [[ -e "${BASE_FOLDER}/${REPO_NAME}" ]]; then
-  echo "Repo '${REPO_NAME}' already exists in '${BASE_FOLDER}', skipping..."
-  exit 1
-fi
-
-pushd "${BASE_FOLDER}" > /dev/null
-    echo "Entering ${BASE_FOLDER}"
-    git clone "git@${REPO}"
-popd > /dev/null
+# Iterate through the repos in the workspace
+for row in "${!REPOS[@]}"; do
+  _jq(){
+    echo "${REPOS[$row]}" | base64 --decode | jq -r ${1}
+  }
+  repo=$(_jq '.repo')
+  eval base_folder=$(_jq '.base_folder')
+  check_and_clone ${repo} ${base_folder}
+done
